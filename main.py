8a974c978554bd0038145886040a2ee007343ea2
zhiwen <lzw5399@gmail.com>
2020-11-10 11:33:27 +0800

    feat: add yaml reader

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 
+import yaml
+
+main_dir = os.path.split(os.path.realpath(__file__))[0]
+yaml_path = os.path.join(main_dir, "appsettings.yaml")
+
+
+def convert_settings():
+    with open(yaml_path, "r", encoding="utf-8") as f:
+        content = yaml.safe_load(f)
+
+    content["name"] = "中文"
+
+    with open(yaml_path, "w", encoding="utf-8") as f:
+        yaml.dump(content, f, allow_unicode=True)
 
 
 if __name__ == '__main__':
-    print('ok!')
+    convert_settings()
+
